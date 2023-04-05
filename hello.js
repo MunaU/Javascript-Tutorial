dd7488c79c01e9b48ef7156e754dfa05f74a50ad
MunaU <m13475695385@outlook.com>
2023-04-05 15:30:02 -0500

    Basic Javascript

---
+++ hello.js
@@ -0,0 +1,39 @@
+function sayHello(){
+    alert("Hello World");
+}
+//Call function on click of the button
+document.getElementById("myBtn").onclick = sayHello;
+
+function sayHwllo(){
+    alert ("Hello World")
+}
+
+  //Call function on click of the button
+  document.getElementById("myBtn"),onclick = sayHello;
+
+  var x = 5;
+  var y = 10;
+  var sum = x + y;
+  document.write(sum);
+  var myVar = "Hello World"
+  //This is my first JavaScript program
+  document.write("Hello World")
+  var name = "pita"
+  var age = 21;
+  var isMarreied = false;
+  // Declaring Variable
+  var userName;
+
+  // Declaring Variable
+  var userName;
+
+// Assigning value
+userName = "Clark Kent";
+//Declaring multiple Variables
+var name = "Peter Parker", age = 21, isMarried = false;
+
+/* Longer declarations can be written to span
+multiple lines to improve the readability */
+var name = "Peter Parker",
+age = 21
+
