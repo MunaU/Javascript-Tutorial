326030353db525aee5102086cf8822ff833be5c5
MunaU <m13475695385@outlook.com>
2023-05-02 15:43:14 -0500

    Buttons Added

---
+++ work.js
@@ -0,0 +1,11 @@
+function mycountries()
+{
+    var count = ["Japan", "Germany", "France", "Russia", "Spain"];
+    document.write(count.length);
+}
+function mycities()
+{
+    var count = ["Japan", "Germany", "France", "Russia", "Spain"];
+    document.write(count.length);
+    var removed = count.splice(0,1);
+}
